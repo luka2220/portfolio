1089d9c554cd014b2aafda9420a1480d645b37f5
Luka Piplica <piplicaluka64@gmail.com>
2025-02-18 22:20:11 -0500

    Icon switch working

---
+++ components/header.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { DynamicIcon } from 'lucide-react/dynamic';
+import React, { useState } from 'react';
+
+type Theme = 'sun' | 'moon';
+
+export default function Header() {
+  const [theme, setTheme] = useState<Theme>('sun');
+
+  function updateTheme() {
+    setTheme((prevTheme) => (prevTheme === 'sun' ? 'moon' : 'sun'));
+  }
+
+  return (
+    <header>
+      <div>
+        <h1>Portoflio</h1>
+      </div>
+      <div>
+        <DynamicIcon name={theme} onClick={updateTheme} />
+      </div>
+    </header>
+  );
+}
