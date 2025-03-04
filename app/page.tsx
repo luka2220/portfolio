8bd92e2ed71b57e53ff3dcc0921acf433a256b53
Luka Piplica <42144047+luka2220@users.noreply.github.com>
2025-03-03 20:44:37 -0500

    Text styling update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <>
-      {/* Hero section  */}
       <section className="w-full min-h-screen flex flex-col justify-center px-6 md:px-16 lg:max-w-4xl mx-auto">
-        <h3 className="mb-4 text-gray-800 dark:text-gray-400 lg:text-2xl md:text-2xl sm:text-xl">
+        <h3 className="mb-4 text-gray-800 dark:text-gray-400 para-text leading-[1.45]">
           Hey, there 👋
         </h3>
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight text-gray-800 dark:text-gray-400">
@@ -14,7 +13,7 @@ export default function Home() {
             a Software Engineer
           </span>
         </h1>
-        <h3 className="mt-3 text-xl sm:text-xl md:text-2xl leading-[1.45] text-gray-800 dark:text-gray-400">
+        <h3 className="mt-3 para-text leading-[1.45] text-gray-800 dark:text-gray-400">
           Specializing in designing, building, and optimizing full-stack web
           applications.
         </h3>
@@ -24,8 +23,8 @@ export default function Home() {
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-semibold text-gray-800 dark:text-name">
           A little bit about me
         </h1>
-        <h3 className="my-5 text-gray-800 dark:text-gray-400 text-center lg:text-2xl md:text-2xl sm:text-xl leading-7">
-          I am a software developer passionate about building software and
+        <h3 className="my-5 text-gray-800 dark:text-gray-400 text-center para-text leading-loose">
+          I am a software engineer passionate about building software and
           contributing to open-source projects and tools. My main programming
           languages are TypeScript and JavaScript, however I love learning new
           technologies and digging deep into how things run and work at a low
